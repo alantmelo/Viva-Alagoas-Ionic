3335bcc52fa7c8c46d30c26c49e56482ba97ecbc
Alan Melo <alantmelo@gmail.com>
2024-10-07 08:17:34 -0300

    [fix] login button

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,11 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
-
+  constructor(private router: Router) {}
+  register() {
+    this.router.navigate(['/register']); // Navega para a página 'detail'
+  }
+  login() {
+    this.router.navigate(['/login']); // Navega para a página 'detail'
+  }
 }
